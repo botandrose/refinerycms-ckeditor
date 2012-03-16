ebc2fd4fa3a3539a24453f3d0fefb306cc943416
Micah Geisel <micah@botandrose.com>
2012-03-16 14:58:53 -0700

    add ckeditor cell plugin and cell processing middleware.

--- lib/refinerycms-ckeditor.rb
+++ lib/refinerycms-ckeditor.rb
@@ -2,6 +2,6 @@ require "refinery/ckeditor/version"
 
 module Refinery
   module CKEditor
-    class Engine < Rails::Engine; end if defined?(Rails::Engine)
+    class Engine < Rails::Engine; end
   end
 end
